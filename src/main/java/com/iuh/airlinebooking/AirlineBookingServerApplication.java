ed73b71ea7a1e8a2148632292870333d28f020c8
Phạm Lê Khánh Duy <phamduy2408@gmail.com>
2023-08-20 03:55:35 +0700

    init code

---
+++ src/main/java/com/iuh/airlinebooking/AirlineBookingServerApplication.java
@@ -0,0 +1,13 @@
+package com.iuh.airlinebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBookingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineBookingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iuh/airlinebooking/AirlineBookingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.airlinebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBookingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
